f3e3223cf0dfc8dafbbaed0ad1749f14bc34c515
tHeJeff0 <xie0144@163.com>
2024-07-16 18:13:12 +0800

    租金结算

--- GoldenMansion/Assets/Scripts/RoundEndPanelController.cs
+++ GoldenMansion/Assets/Scripts/RoundEndPanelController.cs
@@ -1,3 +1,4 @@
+using ExcelData;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,20 @@ using UnityEngine;
 public class RoundEndPanelController : MonoBehaviour
 {
     [SerializeField] private GameObject quitBuildButton;
+    GameObject commitRentGroup;
+    GameObject buildButton;
+    GameObject nextLevelButton;
+
+    private void Awake()
+    {
+        commitRentGroup = GameObject.Find("CommitRentGroup");
+        buildButton = GameObject.Find("BuildButton");
+        nextLevelButton = GameObject.Find("NextLevelButton");
+
+        commitRentGroup.SetActive(true);
+        buildButton.SetActive(false);
+        nextLevelButton.SetActive(false);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -43,5 +58,22 @@ public class RoundEndPanelController : MonoBehaviour
     {
         Destroy(this.gameObject);
         GameManager.Instance.levelKey += 1;
+    }
+
+    public void CommitRent()
+    {
+        float vaultMoney = ApartmentController.Instance.vaultMoney;
+        float targetMoney = Level.GetItem(GameManager.Instance.levelKey).target;
+        float moneyLeft = vaultMoney - targetMoney;
+        if (moneyLeft >= 0)
+        {
+            buildButton.SetActive(true);
+            nextLevelButton.SetActive(true);
+            commitRentGroup.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Game Over!");
+        }
     }
 }
