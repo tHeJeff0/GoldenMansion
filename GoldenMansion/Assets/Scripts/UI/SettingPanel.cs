2fc59e7da799881ed3c4ee8dbe5fba03e3110ab3
tHeJeff0 <xie0144@163.com>
2024-11-24 18:04:39 +0800

    增加分辨率设置

--- GoldenMansion/Assets/Scripts/UI/SettingPanel.cs
+++ GoldenMansion/Assets/Scripts/UI/SettingPanel.cs
@@ -69,14 +69,57 @@ public class SettingPanel : MonoBehaviour
         {
             
             case 0:
-                Screen.SetResolution(715, 402, Screen.fullScreen);
+                Screen.SetResolution(462, 174, Screen.fullScreen);
                 SaveSystem.Instance.LoadPlayerPrefs();
                 break;
             case 1:
+                Screen.SetResolution(715, 402, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 2:
+                Screen.SetResolution(1024, 600, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 3:
+                Screen.SetResolution(1024, 768, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 4:
+                Screen.SetResolution(1280, 800, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 5:
+                Screen.SetResolution(1280, 1024, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 6:
                 Screen.SetResolution(1366, 768, Screen.fullScreen);
                 SaveSystem.Instance.LoadPlayerPrefs();
                 break;
-                
+            case 7:
+                Screen.SetResolution(1440, 1050, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 8:
+                Screen.SetResolution(1600, 900, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 9:
+                Screen.SetResolution(1600, 1200, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 10:
+                Screen.SetResolution(1680, 1050, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 11:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
+            case 12:
+                Screen.SetResolution(1920, 1200, Screen.fullScreen);
+                SaveSystem.Instance.LoadPlayerPrefs();
+                break;
         }       
     }
 
