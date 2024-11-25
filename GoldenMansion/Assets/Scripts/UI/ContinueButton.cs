5bc4a22b8076602e208a5f943a22a5d3969c7bc6
tHeJeff0 <xie0144@163.com>
2024-11-25 17:59:29 +0800

    调整文本配置,调整角色职业描述ui，调整设置界面ui

--- GoldenMansion/Assets/Scripts/UI/ContinueButton.cs
+++ GoldenMansion/Assets/Scripts/UI/ContinueButton.cs
@@ -13,11 +13,11 @@ public class ContinueButton : MonoBehaviour
     {
         continueButton = GetComponent<Button>();
         continueButton.interactable = false;
-        continueButton.GetComponentInChildren<TextMeshProUGUI>().color = new Color(0, 0, 0, 0.5f);    
+        continueButton.GetComponentInChildren<TextMeshProUGUI>().color = new Color(0.16f, 0, 0, 0.5f);    
         if(File.Exists(Path.Combine(Application.persistentDataPath, "save.sav")))
         {
             continueButton.interactable = true;
-            continueButton.GetComponentInChildren<TextMeshProUGUI>().color = new Color(0, 0, 0, 255);
+            continueButton.GetComponentInChildren<TextMeshProUGUI>().color = new Color(0.16f, 0, 0, 1.0f);
         }
         
     }
